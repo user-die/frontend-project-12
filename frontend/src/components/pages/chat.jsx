99ae7696197a5781c1d58cfdee798e5d3bc9bac2
user-die <vadim.vadimov123@mail.ru>
2023-11-27 23:36:48 +0500

    1

--- frontend/src/components/pages/chat.jsx
+++ frontend/src/components/pages/chat.jsx
@@ -1,26 +1,27 @@
 /* eslint-disable */
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useTranslation } from 'react-i18next';
-import { useContext, useEffect, useState } from 'react';
-import filter from 'leo-profanity';
-import axios from 'axios';
-import * as yup from 'yup';
-import { useFormik } from 'formik';
-import uniqueId from 'lodash.uniqueid';
-import { io } from 'socket.io-client';
-import MyContext from '../MyContext';
-import AddChannel from '../addChannel';
-import RenameChannel from '../renameChannel';
-import RemoveChannel from '../removeChannel';
-import ChangeLanguage from '../changeLanguage';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useTranslation } from "react-i18next";
+import { useContext, useEffect, useState } from "react";
+import filter from "leo-profanity";
+import axios from "axios";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import uniqueId from "lodash.uniqueid";
+import { io } from "socket.io-client";
+import MyContext from "../MyContext";
+import AddChannel from "../addChannel";
+import RenameChannel from "../renameChannel";
+import RemoveChannel from "../removeChannel";
+import ChangeLanguage from "../changeLanguage";
+import ChangeTheme from "../changeTheme";
 
 const socket = io();
 
 export default function Chat() {
   const [messages, setMessages] = useState();
   const [modal, setModal] = useState();
-  const [channel, setChannel] = useState({ name: 'general', id: 1 });
+  const [channel, setChannel] = useState({ name: "general", id: 1 });
   const [channels, setChannels] = useState();
   const [rename, setRename] = useState(false);
   const [remove, setRemove] = useState();
@@ -29,15 +30,15 @@ export default function Chat() {
   const { t } = useTranslation();
   const loginData = useContext(MyContext);
 
-  const created = () => toast(t('channel created'));
-  const renamed = () => toast(t('channel renamed'));
-  const deleted = () => toast(t('channel deleted'));
-  const serverError = () => toast.error(t('serverError'));
+  const created = () => toast(t("channel created"));
+  const renamed = () => toast(t("channel renamed"));
+  const deleted = () => toast(t("channel deleted"));
+  const serverError = () => toast.error(t("serverError"));
 
   useEffect(() => {
     const requestData = async () => {
       await axios
-        .get('/api/v1/data', {
+        .get("/api/v1/data", {
           headers: {
             Authorization: `Bearer ${localStorage.token}`,
           },
@@ -56,24 +57,24 @@ export default function Chat() {
   function del(id) {
     setChannels((channels) => channels.filter((el) => el.id !== id));
 
-    setChannel({ name: 'general', id: 1 });
+    setChannel({ name: "general", id: 1 });
   }
 
   useEffect(() => {
-    socket.on('newMessage', (payload) => {
+    socket.on("newMessage", (payload) => {
       setMessages((messages) => [...messages, payload]);
     });
 
-    socket.on('newChannel', (payload) => {
+    socket.on("newChannel", (payload) => {
       setChannels((channels) => [...channels, payload]);
       setChannel({ name: payload.name, id: payload.id });
     });
 
-    socket.on('removeChannel', (payload) => {
+    socket.on("removeChannel", (payload) => {
       del(payload.id, channels);
     });
 
-    socket.on('renameChannel', (payload) => {
+    socket.on("renameChannel", (payload) => {
       setChannels((channels) => {
         const copyChannels = [...channels];
         copyChannels.splice(
@@ -81,7 +82,7 @@ export default function Chat() {
             copyChannels.filter((el) => el.id == payload.id)[0]
           ),
           1,
-          payload,
+          payload
         );
         return copyChannels;
       });
@@ -90,13 +91,13 @@ export default function Chat() {
 
   const formikForMessage = useFormik({
     initialValues: {
-      message: '',
+      message: "",
     },
     onSubmit: (values, { resetForm }) => {
-      if (values.message !== '') {
+      if (values.message !== "") {
         const message = filter.clean(values.message);
 
-        socket.emit('newMessage', {
+        socket.emit("newMessage", {
           body: message,
           channelId: channel.id,
           username: loginData.nickname,
@@ -105,7 +106,7 @@ export default function Chat() {
       }
     },
     validationSchema: yup.object().shape({
-      message: yup.string().required(t('enter a message')),
+      message: yup.string().required(t("enter a message")),
     }),
   });
 
@@ -125,80 +126,83 @@ export default function Chat() {
   }
 
   function quite() {
-    loginData.setLogin('notLogin');
+    loginData.setLogin("notLogin");
   }
 
   function changeChannel(e) {
     setChannel({
-      name: e.target.textContent.replace('#', ''),
+      name: e.target.textContent.replace("#", ""),
       id: e.target.id,
     });
   }
 
-  const body = document.querySelector('body');
+  const body = document.querySelector("body");
 
   function openAddChannel() {
-    body.className = 'h-100 bg-light modal-open';
+    body.className = "h-100 modal-open";
     setModal(true);
   }
 
   function closeAddChannel() {
-    body.className = 'h-100 bg-light';
+    body.className = "h-100";
     setModal(false);
   }
 
   function dropMenu(e) {
-    if (e.target.nextElementSibling.className === 'dropdown-menu') {
-      e.target.nextElementSibling.className = 'dropdown-menu show';
+    if (e.target.nextElementSibling.className === "dropdown-menu") {
+      e.target.nextElementSibling.className = "dropdown-menu show";
     } else {
-      e.target.nextElementSibling.className = 'dropdown-menu';
+      e.target.nextElementSibling.className = "dropdown-menu";
     }
   }
 
   function openRename(e) {
-    body.className = 'h-100 bg-light modal-open';
+    body.className = "h-100 modal-open";
     setRename(true);
     setId(e.target.id);
   }
 
   function closeRename() {
-    body.className = 'h-100 bg-light';
+    body.className = "h-100";
     setRename(false);
   }
 
   function openRemove(id) {
-    body.className = 'h-100 bg-light modal-open';
+    body.className = "h-100 modal-open";
     setId(id);
     setRemove(true);
   }
 
   function closeRemove() {
-    body.className = 'h-100 bg-light';
+    body.className = "h-100";
     setRemove(false);
   }
 
   return (
     <div id="chat" className="h-100">
       <div className="d-flex flex-column h-100">
-        <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+        <nav className="shadow-sm navbar navbar-expand-lg navbar-light">
           <div className="container d-block">
             <div className="row">
-              <div className="col-md-10">
+              <div className="col-9">
                 <a className="navbar-brand" href="/">
                   Chat Slack
                 </a>
               </div>
-              <div className="col-md-1">
+              <div className="col-1">
+                <ChangeTheme />
+              </div>
+              <div className="col-1">
                 <ChangeLanguage />
               </div>
-              <div className="col-md-1">
-                {' '}
+              <div className="col-1">
+                {" "}
                 <button
                   onClick={quite}
                   type="button"
                   className="btn btn-primary"
                 >
-                  {t('log out')}
+                  {t("log out")}
                 </button>
               </div>
             </div>
@@ -206,10 +210,10 @@ export default function Chat() {
         </nav>
         <ToastContainer />
         <div className="container h-100 my-4 overflow-hidden rounded shadow">
-          <div className="row h-100 bg-white flex-md-row">
-            <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
+          <div className="row h-100 flex-md-row">
+            <div className="col-4 col-md-2 border-end px-0 flex-column h-100 d-flex">
               <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
-                <b>{t('channels')}</b>
+                <b>{t("channels")}</b>
                 <button
                   type="button"
                   className="p-0 text-primary btn btn-group-vertical"
@@ -232,7 +236,8 @@ export default function Chat() {
                 id="channels-box"
                 className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block"
               >
-                {channels && channels.map((el) => (
+                {channels &&
+                  channels.map((el) => (
                     <li key={uniqueId()} className="nav-item w-100">
                       <div
                         className="d-flex dropdown btn-group"
@@ -243,8 +248,8 @@ export default function Chat() {
                           id={el.id}
                           className={
                             channel.name === `${el.name}`
-                              ? 'w-100 rounded-0 text-start btn btn-secondary'
-                              : 'w-100 rounded-0 text-start btn'
+                              ? "w-100 rounded-0 text-start btn btn-secondary"
+                              : "w-100 rounded-0 text-start btn"
                           }
                           onClick={changeChannel}
                         >
@@ -293,17 +298,17 @@ export default function Chat() {
                             href="#"
                             onClick={openRename}
                           >
-                            {t('rename')}
+                            {t("rename")}
                           </a>
                         </div>
                       </div>
                     </li>
-                ))}
+                  ))}
               </ul>
             </div>
             <div className="col p-0 h-100">
               <div className="d-flex flex-column h-100">
-                <div className="bg-light mb-4 p-3 shadow-sm small">
+                <div className="mb-4 p-3 shadow-sm small">
                   <p className="m-0">{channel && <b>{channel.name}</b>}</p>
                   {channel && messages && (
                     <span className="text-muted">
@@ -312,11 +317,11 @@ export default function Chat() {
                           .length
                       }
                       &nbsp;
-                      {t('message', {
+                      {t("message", {
                         context: declinationOfMessage(
                           messages.filter((el) => el.channelId == channel.id)
                             .length,
-                          ['sy', 's', false]
+                          ["sy", "s", false]
                         ),
                       })}
                     </span>
@@ -326,14 +331,14 @@ export default function Chat() {
                   id="messages-box"
                   className="chat-messages overflow-auto px-5"
                 >
-                  {messages && messages.filter((el) => el.channelId == channel.id)
-                    .map((message) => (
-                      <div key={uniqueId()} className="text-break mb-2">
-                        <b>{message.username}</b>
-                        :
-                        {message.body}
-                      </div>
-                    ))}
+                  {messages &&
+                    messages
+                      .filter((el) => el.channelId == channel.id)
+                      .map((message) => (
+                        <div key={uniqueId()} className="text-break mb-2">
+                          <b>{message.username}</b>:{message.body}
+                        </div>
+                      ))}
                 </div>
                 <div className="mt-auto px-5 py-3">
                   <form
@@ -344,7 +349,7 @@ export default function Chat() {
                       <input
                         name="message"
                         aria-label="Новое сообщение"
-                        placeholder={t('enter a message')}
+                        placeholder={t("enter a message")}
                         className="border-0 p-0 ps-2 form-control"
                         value={formikForMessage.values.message}
                         onChange={formikForMessage.handleChange}
@@ -362,7 +367,7 @@ export default function Chat() {
                             d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
                           />
                         </svg>
-                        <span className="visually-hidden">{t('submit')}</span>
+                        <span className="visually-hidden">{t("submit")}</span>
                       </button>
                     </div>
                   </form>
