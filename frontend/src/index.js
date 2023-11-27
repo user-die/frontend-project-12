8caacf4f19c7f81c80f64e21231cfc8a194e34fd
user-die <vadim.vadimov123@mail.ru>
2023-11-27 17:48:18 +0500

    1

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/umd/popper.js';
 import './components/i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
